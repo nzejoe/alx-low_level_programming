918a06c6fd25bc4df746bb82643588a91fdc65fc
nzejoe <jonathannzete@gmail.com>
2023-07-13 13:23:02 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#!/bin/bash
-#include <stdio.h>
-void main(){
-	puts("\"Programming is like building a multilingual puzzle")
-	return 0
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
