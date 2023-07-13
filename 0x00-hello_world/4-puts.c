0c76b37573bc3fe8ed05944731dfd75c4c374732
nzejoe <jonathannzete@gmail.com>
2023-07-13 13:18:46 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #!/bin/bash
 #include <stdio.h>
 void main(){
-	put("\"Programming is like building a multilingual puzzle")
+	puts("\"Programming is like building a multilingual puzzle")
 	return 0
 }
