4f502c2e898a9aa3a97077c6bfbe4c355236cee3
nzejoe <jonathannzete@gmail.com>
2023-07-13 13:38:10 -0700

    prinf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - print string to the screen with printf
+ * Return: 0 (Does not return anything than zero (0))
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
