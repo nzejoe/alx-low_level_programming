02043bce0816b752764fd9e13c67b2594962ce35
nzejoe <jonathannzete@gmail.com>
2023-07-14 15:12:46 -0700

    print numbers with putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
+
 /**
- * main - print base 10 numbers from 0
- *
- * Return: 0
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n = 0;
+	int n;
 
-	while (n < 10)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-		n++;
 	}
 	putchar('\n');
 	return (0);
