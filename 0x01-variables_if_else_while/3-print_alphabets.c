3242a9556f1e5d067cb98036626b0b4d46577f46
nzejoe <jonathannzete@gmail.com>
2023-07-14 14:24:51 -0700

    print alphabets in lower and upper case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,7 @@ int main(void)
 	for (ch = 'a'; ch <= 'z'; ch++)
 		pustchar(ch);
 	for (ch = 'A'; ch <= 'Z'; ch++)
-                pustchar(ch);
+                putchar(ch);
 	putchar('\n');
 	return (0);
 }
