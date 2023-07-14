4dba4421e4b7f601c666f11d998284bab9a8422d
nzejoe <jonathannzete@gmail.com>
2023-07-14 14:52:56 -0700

    print alphabets in lower and upper case

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.io>
+/**
+ * main - print alphabets except q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char ch = 'a';
+
+while (ch <= 'z')
+{
+if (ch != 'q' || ch != 'e')
+{
+putchar(ch);
+}
+ch++;
+}
+putchar('\n');
+return (0);
+}
+
