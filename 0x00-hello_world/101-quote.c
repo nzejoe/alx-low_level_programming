1519509f5ff469ce62e8d1438dd35e5a26e46d69
nzejoe <jonathannzete@gmail.com>
2023-07-13 19:24:59 -0700

    using unistd

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - prints to the screen without using standard out funtions
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
