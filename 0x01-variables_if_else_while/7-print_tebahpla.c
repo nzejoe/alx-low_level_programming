1f73cae79b0b4738fe85bb33df407dc31ce425ac
nzejoe <jonathannzete@gmail.com>
2023-07-14 15:25:09 -0700

    print alphabet reversed

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	char ch = 'z';
 
-while (ch >= 'a')
-{
-putchar(ch);
-z--;
-}
-putchar('\n');
-return (0);
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar('\n');
+	return (0);
 }
 
