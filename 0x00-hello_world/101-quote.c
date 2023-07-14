e2f32f362ac5b4f82d7ef637f89ec9e31b51dead
nzejoe <jonathannzete@gmail.com>
2023-07-13 19:33:59 -0700

    using unistd

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,9 @@
 /**
  * main - prints to the screen without using standard out funtions
  * Return: 0 (success)
-*/
+ */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
