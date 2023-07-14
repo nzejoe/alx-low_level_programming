b4935bbc1d1708431f33248aba7291969a6eb995
nzejoe <jonathannzete@gmail.com>
2023-07-14 14:15:49 -0700

    print alphabets to the screen

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print alphabets to the screen
  *
@@ -7,11 +8,12 @@
 int main(void)
 {
 	char ch;
-	for(ch = 'a'; ch <= 'z'; ch++)
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		putchar(ch);
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
 
