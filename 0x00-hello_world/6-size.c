59d36e3991d46d7ba74028a8a1b4c675a1857bae
nzejoe <jonathannzete@gmail.com>
2023-07-13 18:42:26 -0700

    print data type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,18 +2,20 @@
 /**
  * main - prints sizes of data type allocation
  *
- * Return: 0 (Return nothing but zero (0)
+ * Return: 0 (Return nothing but zero (0))
 */
-int main(void){
+int main(void)
+{
 	char a;
 	int b;
 	long int c;
 	long long int d;
 	float e;
-	printf("Size of a char: %lu byte(s\n)", (unasigned long)sizeof(a));
-	printf("Size of a int: %lu byte(s)\n", (unasigned long)sizeof(b));
-	printf("Size of a long int: %lu byte(s)\n", (unasigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(d));
-	printf("Size of a float: %lu byte(s)", (unasigned long)sizeof(f));
-	return 0;
+
+	printf("Size of a char: %lu byte(s\n)", (unsigned long)sizeof(a));
+	printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
 }
