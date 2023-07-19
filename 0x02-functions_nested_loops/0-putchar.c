34efef9a157a812c901043f48767e2f7dcf22a52
nzejoe <jonathannzete@gmail.com>
2023-07-18 18:25:58 -0700

    print character to screen using _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * main - main function prints single character to the screen using _putchar prototype function
+ * main - main function prints single character to the screen
+ * using _putchar prototype function
  *
  * Return: 0 - just return o
  */
 int main(void)
 {
-	_putchar('_');
-    	_putchar('p');
-    	_putchar('u');
-    	_putchar('t');
-   	_putchar('c');
-    	_putchar('h');
-    	_putchar('a');
-    	_putchar('r');
-    	_putchar('\n');
-    	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
 
