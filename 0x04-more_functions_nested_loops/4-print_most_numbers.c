32a973e71ad5cb8312e41873d7b81f4cda09ec18
nzejoe <jonathannzete@gmail.com>
2023-07-20 19:14:52 -0700

    print numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks for checks for a digit (0 through 9) and print number if it is not 2 and 4.
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		if(c != 2 && c != 4)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
+
