6ae0533ea7feae765c38b4444a67066a753e8ed6
nzejoe <jonathannzete@gmail.com>
2023-07-20 19:17:43 -0700

    print numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0.
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int c;
 
 	for (c = 48; c < 58; c++)
 	{
-		if(c != 2 && c != 4)
+		if (c != 2 && c != 4)
 			_putchar(c);
 	}
 	_putchar('\n');
