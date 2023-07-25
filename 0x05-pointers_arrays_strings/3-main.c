55ada44bdaf3edfb39a8e35b45773f4c4aa3c8a9
nzejoe <jonathannzete@gmail.com>
2023-07-25 10:36:43 -0700

    prints string followed by new line

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,17 @@
+julien@ubuntu:~/0x05$ cat 3-main.c
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
+}
+
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+* _puts - prints string followed by new line
+* @str: string argument
+* Return: nothing
+*/
+void _puts(char *str)
+{
+	while (*str < '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
+
