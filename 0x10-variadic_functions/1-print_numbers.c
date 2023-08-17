a3ce9d91a4a6137f2e5bd3b274a26973f22c29d8
nzejoe <jonathannzete@gmail.com>
2023-08-17 11:29:42 -0700

    function that prints numbers, followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,26 +1,30 @@
 #include "variadic_functions.h"
+#include <stdio.h>
 #include <stdarg.h>
 
 /**
- * sum_them_all - Returns the sum of all its paramters.
- * @n: The number of paramters passed to the function.
- * @...: A variable number of paramters to calculate the sum of.
- *
- * Return: If n == 0 - 0.
- *         Otherwise - the sum of all parameters.
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
  */
-int sum_them_all(const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i, sum = 0;
+	va_list nums;
+	unsigned int index;
 
-	va_start(ap, n);
+	va_start(nums, n);
 
-	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(nums, int));
 
-	va_end(ap);
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
 
-	return (sum);
+	printf("\n");
+
+	va_end(nums);
 }
 
